4c783bce9eb35b963baba22442d0da84726e6160
FLeivaTraza <fleiva@trazalog.com>
2019-01-30 20:02:43 -0300

    Primera Versión de Programa

---
+++ FileListenerTest/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FileListenerTest
+{
+    class Data
+    {
+        public String registro;
+        public Data(String data) {
+            this.registro = data;
+        }
+    }
+}
---
+++ FileListenerTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.Permissions;
+namespace FileListenerTest
+{
+    class Program
+    {
+        static String file = "F:\\usuarios\\alumno\\Documentos\\Visual Studio 2008\\Projects\\FileListenerTest\\FileListenerTest\\bin\\Debug\\salida.txt";
+        static Registro R;
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Proyecto Ansilta: Registros del Archivo Salida");
+            R = new Registro(file);
+            while (true)
+            {
+                if (R.Actualizar()) //Detecta cambios en el archivo
+                {
+
+                    //R.Mostrar_Registros();
+                    //R.ReenviarRegistrosTemporales();
+                    //R.EnviarNuevosRegistros();
+                }
+                System.Threading.Thread.Sleep(15000);
+                R.backup = false;
+            }
+        }
+    }
+}
