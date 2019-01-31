1fed59059fc515ffac398c4aee02fcc3d960d0a4
FLeivaTraza <fleiva@trazalog.com>
2019-01-30 21:39:14 -0300

    Envio Registros en formato correspondiente

--- FileListenerTest/Data.cs
+++ FileListenerTest/Data.cs
@@ -7,9 +7,16 @@ namespace FileListenerTest
 {
     class Data
     {
-        public String registro;
+        public String codigo;
+        public int cantidad;
+        public String tipo;
+        public int orden_corte;
         public Data(String data) {
-            this.registro = data;
+            String[] aux = data.Split(',');
+            this.codigo = aux[0];
+            this.cantidad = Int16.Parse(aux[1]);
+            this.tipo = aux[2];
+            this.orden_corte = Int32.Parse(aux[3]);
         }
     }
 }
